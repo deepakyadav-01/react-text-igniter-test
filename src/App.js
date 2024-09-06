1908d3bfad0e726a9245ae4505dcbf468404593f
Deepak Yadav <deepak.y@mindfiresolutions.com>
2024-09-06 18:22:13 +0530

    feat:test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { TextIgniter } from "@mindfiredigital/react-text-igniter";
+import React, { useRef } from "react";
+import "./App.css";
+export default function App() {
+  const editorRef = useRef();
 
-function App() {
+  // define features
+  const features = [
+    "heading",
+    "bold",
+    "italic",
+    "underline",
+    "orderedList",
+    "unorderedList",
+    "justifyLeft",
+    "justifyCenter",
+    "justifyRight",
+    "createLink",
+    "insertImage",
+    "superscript",
+    "subscript",
+    "table",
+    "layout",
+  ];
+
+  // get HTML content
+  const handleGetHtmlContent = () => {
+    console.log("HTML:", editorRef.current.getHtml());
+  };
+
+  // get JSON content
+  const handleGetJsonContent = () => {
+    console.log("JSON:", editorRef.current.getJson());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <TextIgniter ref={editorRef} features={features} height={"200px"} />
+      <div className="content-div">
+        <p>Get Content ( check browser console)</p>
+        <button className="html-btn" onClick={handleGetHtmlContent}>
+          check html
+        </button>
+        <button className="json-btn" onClick={handleGetJsonContent}>
+          check Json
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
